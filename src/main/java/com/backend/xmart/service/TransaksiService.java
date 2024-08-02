eef1fc60237e10ede748f5d1f8c88b02237c1bc2
Irawancoy <septyoirawan8@gmail.com.Irawancoy>
2024-08-02 10:45:36 +0700

    mengurangi warning duplikasi sonarqube

--- src/main/java/com/backend/xmart/service/TransaksiService.java
+++ src/main/java/com/backend/xmart/service/TransaksiService.java
@@ -13,20 +13,16 @@ import java.util.*;
 import java.util.stream.Collectors;
 import org.springframework.data.domain.Page;
 
-@Service // Anotasi ini menandakan bahwa kelas ini adalah service dan akan dikelola oleh Spring.
+@Service
 public class TransaksiService {
 
-   @Autowired // Menyuntikkan instance TransaksiRepository ke dalam TransaksiService.
+   @Autowired
    private TransaksiRepository transaksiRepository;
 
-   // Metode untuk mendapatkan semua transaksi dengan pagination
    public ResponseEntity<ResponseBody> getAllTransaksi(Pageable pageable) {
       ResponseBody responseBody = new ResponseBody();
       try {
-         // Mengambil data transaksi secara paginated dari repository
          Page<TransaksiModel> transaksi = transaksiRepository.findAll(pageable);
-         
-         // Mengubah List<TransaksiModel> menjadi List<TransaksiResponse> untuk dikembalikan sebagai response
          List<TransaksiResponse> response = transaksi.stream().map(trans -> {
             TransaksiResponse transaksiResponse = new TransaksiResponse();
             transaksiResponse.setNamaCustomer(trans.getCustomer().getNama());
@@ -36,23 +32,16 @@ public class TransaksiService {
             transaksiResponse.setDate(trans.getDate());
             return transaksiResponse;
          }).collect(Collectors.toList());
-
-         // Mengisi ResponseBody dengan data dan informasi tambahan
          responseBody.setTotal(transaksiRepository.count());
          responseBody.setData(response);
          responseBody.setMessage("Daftar Transaksi Berhasil Ditampilkan");
          responseBody.setStatusCode(HttpStatus.OK.value());
          responseBody.setStatus(HttpStatus.OK.name());
-         
-         // Mengembalikan response dengan status OK
          return ResponseEntity.ok(responseBody);
       } catch (Exception e) {
-         // Menangani kesalahan dan mengisi ResponseBody dengan informasi kesalahan
          responseBody.setMessage(e.getMessage());
          responseBody.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
          responseBody.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.name());
-         
-         // Mengembalikan response dengan status INTERNAL_SERVER_ERROR
          return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseBody);
       }
    }
